f903ac8c9d2fc6212ccd21e841295141b4da4297
Patrick Nast <pnast@uw.edu>
2025-03-17 16:08:36 -0700

    modify tests

--- src/vivarium_testing_utils/automated_validation/data_loader.py
+++ src/vivarium_testing_utils/automated_validation/data_loader.py
@@ -21,9 +21,13 @@ class DataLoader:
         self.metadata = LayeredConfigTree()
         self.artifact = None  # Just stubbing this out for now
 
-    def load_from_source(self, dataset_key: str, source: str) -> None:
-        """Load the data from the given source via the loader mapping."""
-        return self.loader_mapping[source](dataset_key)
+    def get_sim_outputs(self) -> list[str]:
+        """Get a list of the datasets in the given simulation output directory.
+        Only return the filename, not the extension."""
+        return [str(f.stem) for f in self.sim_output_dir.glob("*.parquet")]
+
+    def get_artifact_keys(self) -> list[str]:
+        raise NotImplementedError
 
     def get_dataset(self, dataset_key: str, source: str) -> pd.DataFrame:
         """Return the dataset from the cache if it exists, otherwise load it from the source."""
@@ -34,17 +38,13 @@ class DataLoader:
             self.add_to_datasets(dataset_key, source, dataset)
             return dataset
 
+    def load_from_source(self, dataset_key: str, source: str) -> None:
+        """Load the data from the given source via the loader mapping."""
+        return self.loader_mapping[source](dataset_key)
+
     def add_to_datasets(self, dataset_key: str, source: str, data: pd.DataFrame) -> None:
         """Update the raw_datasets cache with the given data."""
         self.raw_datasets.update({source: {dataset_key: data}})
-
-    def get_sim_outputs(self) -> list[str]:
-        """Get a list of the datasets in the given simulation output directory.
-        Only return the filename, not the extension."""
-        return [str(f.stem) for f in self.sim_output_dir.glob("*.parquet")]
-
-    def get_artifact_keys(self) -> list[str]:
-        raise NotImplementedError
 
     def load_from_sim(self, dataset_key: str) -> pd.DataFrame:
         """Load the data from the simulation output directory and set the non-value columns as indices."""
--- tests/automated_validation/test_data_loader.py
+++ tests/automated_validation/test_data_loader.py
@@ -1,16 +1,56 @@
 from vivarium_testing_utils.automated_validation.data_loader import DataLoader
+import pytest
+from unittest.mock import MagicMock
+import pandas as pd
 
 
-def test_sim_outputs(sim_result_dir):
+def test_get_sim_outputs(sim_result_dir):
+    """Test we have the correctly truncated sim data keys"""
     data_loader = DataLoader(sim_result_dir)
-    assert data_loader.sim_outputs() == [
+    assert data_loader.get_sim_outputs() == [
         "deaths",
         "person_time_cause",
         "transition_count_cause",
     ]
 
 
-def test_load_from_simulation(sim_result_dir):
+def test_get_dataset(sim_result_dir):
+    """Ensure that we load data from disk if needed, and don't if not."""
+    data_loader = DataLoader(sim_result_dir)
+    # check that we call load_from_source the first time we call get_dataset
+    data_loader.load_from_source = MagicMock()
+    data_loader.get_dataset("deaths", "sim"), pd.DataFrame
+    data_loader.load_from_source.assert_called_once_with("deaths", "sim")
+    # check that we don't call load_from_source the second time we call get_dataset
+    data_loader.load_from_source = MagicMock()
+    data_loader.get_dataset("deaths", "sim"), pd.DataFrame
+    data_loader.load_from_source.assert_not_called()
+
+
+@pytest.mark.parametrize(
+    "dataset_key, source",
+    [
+        ("deaths", "sim"),
+    ],
+)
+def load_from_source(dataset_key, source, sim_result_dir):
+    """Ensure we can sensibly load using key / source combinations"""
+    data_loader = DataLoader(sim_result_dir)
+    assert not data_loader.raw_datasets.get(source).get(dataset_key)
+    data_loader.load_from_source(dataset_key, source)
+    assert data_loader.raw_datasets.get(source).get(dataset_key)
+
+
+def test_add_to_datasets(sim_result_dir):
+    """Ensure that we can add data to the cache"""
+    df = pd.DataFrame({"baz": [1, 2, 3]})
+    data_loader = DataLoader(sim_result_dir)
+    data_loader.add_to_datasets("foo", "bar", df)
+    assert data_loader.raw_datasets.get("bar").get("foo").equals(df)
+
+
+def test_load_from_sim(sim_result_dir):
+    """Ensure that we can load data from the simulation output directory"""
     data_loader = DataLoader(sim_result_dir)
     person_time_cause = data_loader.load_from_sim("deaths")
     assert person_time_cause.shape == (8, 1)
