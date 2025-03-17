cada0685485c54032ba0c1386c069372a4fdce76
Patrick Nast <130876799+patricktnast@users.noreply.github.com>
2025-03-17 15:30:27 -0700

    Generate stubs for Automated V&V Phase 1 (#22)
    * first pass at stubbing
    
    * lint
    
    * refine
    
    * Update src/vivarium_testing_utils/automated_validation/interface.py
    
    Co-authored-by: Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
    
    * change pass to notimplementederror
    
    ---------
    
    Co-authored-by: Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>

---
+++ src/vivarium_testing_utils/automated_validation/interface.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+import pandas as pd
+from layered_config_tree import LayeredConfigTree
+
+from vivarium_testing_utils.automated_validation import plot_utils
+from vivarium_testing_utils.automated_validation.comparison import Comparison
+from vivarium_testing_utils.automated_validation.data_loader import DataLoader
+
+
+class ValidationContext:
+    def __init__(self, results_dir: str | Path, age_groups: pd.DataFrame | None):
+        self.data_loader = DataLoader(results_dir)
+        self.comparisons = LayeredConfigTree()
+
+    def get_sim_outputs(self):
+        return self.data_loader.sim_outputs()
+
+    def get_artifact_keys(self):
+        return self.data_loader.artifact_keys()
+
+    def add_comparison(
+        self, measure_key: str, test_source: str, ref_source: str, stratifications: list[str]
+    ) -> None:
+        test_data = self.data_loader.get_dataset(measure_key, test_source)
+        ref_data = self.data_loader.get_dataset(measure_key, ref_source)
+        self.comparisons.update(
+            [measure_key], Comparison(measure_key, test_data, ref_data, stratifications)
+        )
+
+    def verify(self, comparison_key: str, stratifications: list[str] = []):
+        self.comparisons[comparison_key].verify(stratifications)
+
+    def plot_comparison(self, comparison_key: str, type: str, **kwargs):
+        return plot_utils.plot_comparison(self.comparisons[comparison_key], type, kwargs)
+
+    def generate_comparisons(self):
+        raise NotImplementedError
+
+    def verify_all(self):
+        for comparison in self.comparisons.values():
+            comparison.verify()
+
+    def plot_all(self):
+        raise NotImplementedError
+
+    def get_results(self, verbose: bool = False):
+        raise NotImplementedError
