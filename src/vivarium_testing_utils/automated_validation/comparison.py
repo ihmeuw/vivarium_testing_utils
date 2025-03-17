1ad5826541ba85b7aa47ad2e419d8fd3b468baae
Patrick Nast <130876799+patricktnast@users.noreply.github.com>
2025-03-17 15:30:27 -0700

    Generate stubs for Automated V&V Phase 1 (#22)
    * first pass at stubbing
    
    * lint
    
    * refine
    
    * Update src/vivarium_testing_utils/automated_validation/interface.py
    
    Co-authored-by: Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
    
    * change pass to notimplementederror
    
    ---------
    
    Co-authored-by: Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>

---
+++ src/vivarium_testing_utils/automated_validation/comparison.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+from vivarium_testing_utils.automated_validation.calculations import compute_metric
+
+
+class Comparison:
+    def __init__(
+        self,
+        measure_key: str,
+        test_data: pd.DataFrame,
+        reference_data: pd.DataFrame,
+        stratifications: list[str] = [],
+    ):
+        self.measure = measure_key
+        self.test_data = test_data
+        self.reference_data = reference_data
+        self.computed_comparison = compute_metric(
+            self.test_data, self.reference_data, self.measure
+        )
+        # you need to marginalize out the non-stratified columns as well
+
+    def verify(self, stratifications: list[str]):
+        raise NotImplementedError
+
+    def summarize(self, stratifications: list[str]):
+        raise NotImplementedError
+
+    def heads(self, stratifications: list[str]):
+        raise NotImplementedError
