efd786e4e79c7ac0adf8ebb5b7a555f715158864
Patrick Nast <130876799+patricktnast@users.noreply.github.com>
2025-03-24 13:53:20 -0700

    Load From Artifact (#24)
    * load key from artifact
    
    * minor fixes
    
    * add stub tests
    
    * add test artifact
    
    * lint
    
    * remove top level import
    
    * private attrs

--- src/vivarium_testing_utils/automated_validation/data_loader.py
+++ src/vivarium_testing_utils/automated_validation/data_loader.py
@@ -4,7 +4,9 @@ from enum import Enum
 from pathlib import Path
 
 import pandas as pd
+import yaml
 from layered_config_tree import ConfigurationKeyError, LayeredConfigTree
+from vivarium import Artifact
 
 
 class DataSource(Enum):
@@ -22,57 +24,67 @@ class DataSource(Enum):
 
 
 class DataLoader:
-    def __init__(self, results_dir: str, cache_size_mb: int = 1000):
-        self.results_dir = Path(results_dir)
-        self.sim_output_dir = self.results_dir / "results"
-        self.cache_size_mb = cache_size_mb
-        self.raw_datasets = LayeredConfigTree({data_source: {} for data_source in DataSource})
-        self.loader_mapping = {
+    def __init__(self, sim_output_dir: str, cache_size_mb: int = 1000):
+        self._sim_output_dir = Path(sim_output_dir)
+        self._results_dir = self._sim_output_dir / "results"
+        self._cache_size_mb = cache_size_mb
+        self._raw_datasets = LayeredConfigTree(
+            {data_source: {} for data_source in DataSource}
+        )
+        self._loader_mapping = {
             DataSource.SIM: self._load_from_sim,
             DataSource.GBD: self._load_from_gbd,
             DataSource.ARTIFACT: self._load_from_artifact,
             DataSource.CUSTOM: self._load_custom,
         }
-        self.metadata = LayeredConfigTree()
-        self.artifact = None  # Just stubbing this out for now
+        self._metadata = LayeredConfigTree()
+        self._artifact = self._load_artifact(self._sim_output_dir)
 
     def get_sim_outputs(self) -> list[str]:
         """Get a list of the datasets in the given simulation output directory.
         Only return the filename, not the extension."""
-        return [str(f.stem) for f in self.sim_output_dir.glob("*.parquet")]
+        return [str(f.stem) for f in self._results_dir.glob("*.parquet")]
 
     def get_artifact_keys(self) -> list[str]:
-        raise NotImplementedError
+        return self._artifact.keys
 
     def get_dataset(self, dataset_key: str, source: DataSource) -> pd.DataFrame:
         """Return the dataset from the cache if it exists, otherwise load it from the source."""
         try:
-            return self.raw_datasets[source][dataset_key]
+            return self._raw_datasets[source][dataset_key]
         except ConfigurationKeyError:
             dataset = self._load_from_source(dataset_key, source)
-            self._add_to_datasets(dataset_key, source, dataset)
+            self._add_to_cache(dataset_key, source, dataset)
             return dataset
 
     def _load_from_source(self, dataset_key: str, source: DataSource) -> None:
         """Load the data from the given source via the loader mapping."""
-        return self.loader_mapping[source](dataset_key)
+        return self._loader_mapping[source](dataset_key)
 
-    def _add_to_datasets(
-        self, dataset_key: str, source: DataSource, data: pd.DataFrame
-    ) -> None:
+    def _add_to_cache(self, dataset_key: str, source: str, data: pd.DataFrame) -> None:
         """Update the raw_datasets cache with the given data."""
-        self.raw_datasets.update({source: {dataset_key: data}})
+        self._raw_datasets.update({source: {dataset_key: data}})
 
     def _load_from_sim(self, dataset_key: str) -> pd.DataFrame:
         """Load the data from the simulation output directory and set the non-value columns as indices."""
-        sim_data = pd.read_parquet(self.sim_output_dir / f"{dataset_key}.parquet")
+        sim_data = pd.read_parquet(self._results_dir / f"{dataset_key}.parquet")
         if "value" not in sim_data.columns:
             raise ValueError(f"{dataset_key}.parquet requires a column labeled 'value'.")
         sim_data = sim_data.set_index(sim_data.columns.drop("value").tolist())
         return sim_data
 
+    @staticmethod
+    def _load_artifact(results_dir: str) -> Artifact:
+        model_spec_path = Path(results_dir) / "model_specification.yaml"
+        artifact_path = yaml.safe_load(model_spec_path.open("r"))["configuration"][
+            "input_data"
+        ]["artifact_path"]
+        return Artifact(artifact_path)
+
     def _load_from_artifact(self, dataset_key: str) -> pd.DataFrame:
-        raise NotImplementedError
+        data = self._artifact.load(dataset_key)
+        self._artifact.clear_cache()
+        return data
 
     def _load_from_gbd(self, dataset_key: str) -> pd.DataFrame:
         raise NotImplementedError
