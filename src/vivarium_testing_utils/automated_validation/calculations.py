38db5707bc868f4209a5f49bff5edb14f63cc9be
Patrick Nast <pnast@uw.edu>
2025-03-25 13:33:53 -0700

    add initial set of basis transformations

--- src/vivarium_testing_utils/automated_validation/calculations.py
+++ src/vivarium_testing_utils/automated_validation/calculations.py
@@ -13,13 +13,35 @@ def compute_metric(
     raise NotImplementedError
 
 
-def ratio(numerator, denominator):
-    raise NotImplementedError
+def validate_intermediate_data(intermediate_data: pd.DataFrame) -> pd.DataFrame:
+    # raise if any of the columns are not numeric
+    if not intermediate_data.applymap(lambda x: isinstance(x, (int, float))).all().all():
+        raise ValueError("All value columns must be numeric")
 
 
-def aggregate(data: pd.DataFrame, groupby_cols: list[str]) -> pd.DataFrame:
-    raise NotImplementedError
+def ratio(data: pd.DataFrame, numerator, denominator) -> pd.Series:
+    """Return a series of the ratio of two columns in a DataFrame,
+    where the columns are specified by their names."""
+    return data[numerator] / data[denominator]
 
 
-def linear_combination(coefficients: list[float], data: pd.DataFrame) -> pd.Series:
-    raise NotImplementedError
+def aggregate_sum(data: pd.DataFrame, groupby_cols: list[str]) -> pd.DataFrame:
+    """Aggregate the dataframe over the specified index columns by summing."""
+    return data.groupby(groupby_cols).sum()
+
+
+def stratify(data: pd.DataFrame, stratification_cols: list[str]) -> pd.DataFrame:
+    """Stratify the data by the index columns, summing over everything else. Syntactic sugar for aggregate."""
+    return aggregate_sum(data, stratification_cols)
+
+
+def marginalize(data: pd.DataFrame, marginalize_cols: list[str]) -> pd.DataFrame:
+    """Sum over marginalize columns, keeping the rest. Syntactic sugar for aggregate."""
+    return data.groupby(data.index.names.difference(marginalize_cols)).sum()
+
+
+def linear_combination(
+    data: pd.DataFrame, coeff_a: float, col_a: str, coeff_b: float, col_b: str
+) -> pd.Series:
+    """Return a series that is the linear combination of two columns in a DataFrame."""
+    return (data[col_a] * coeff_a) + (data[col_b] * coeff_b)
---
+++ tests/automated_validation/test_calculations.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import pytest
+
+from vivarium_testing_utils.automated_validation.calculations import (
+    aggregate_sum,
+    linear_combination,
+    ratio,
+    validate_intermediate_data,
+)
+
+
+@pytest.fixture
+def intermediate_data() -> pd.DataFrame:
+    return pd.DataFrame(
+        {
+            "a": [1, 2, 3, 4],
+            "b": [4, 5, 6, 7],
+            "c": [1, 1, 0, 1],
+        },
+        index=pd.MultiIndex.from_tuples(
+            [
+                ("x", 0),
+                ("x", 1),
+                ("y", 0),
+                ("y", 1),
+            ],
+            names=["group", "time"],
+        ),
+    )
+
+
+def test_validate_intermediate_data(intermediate_data: pd.DataFrame) -> None:
+    """Test validating a multi-indexed DataFrame"""
+    validate_intermediate_data(intermediate_data)
+    # test non-numeric column
+    intermediate_data["d"] = ["a", "b", "c", "d"]
+    with pytest.raises(ValueError):
+        validate_intermediate_data(intermediate_data)
+
+
+def test_ratio(intermediate_data: pd.DataFrame) -> None:
+    """Test taking ratio of two columns in a multi-indexed DataFrame"""
+    assert ratio(intermediate_data, "a", "b").equals(
+        pd.Series([1 / 4, 2 / 5, 3 / 6, 4 / 7], index=intermediate_data.index)
+    )
+    assert ratio(intermediate_data, "a", "c").equals(
+        pd.Series([1, 2, float("inf"), 4], index=intermediate_data.index)
+    )
+    # test non-existent column
+    with pytest.raises(KeyError):
+        ratio(intermediate_data, "a", "foo")
+
+
+def test_aggregate(intermediate_data: pd.DataFrame) -> None:
+    """Test aggregating over different combinations of value columns."""
+    assert aggregate_sum(intermediate_data, ["group"]).equals(
+        pd.DataFrame(
+            {
+                "a": [3, 7],
+                "b": [9, 13],
+                "c": [2, 1],
+            },
+            index=pd.Index(["x", "y"], name="group"),
+        )
+    )
+    assert aggregate_sum(intermediate_data, ["time"]).equals(
+        pd.DataFrame(
+            {
+                "a": [4, 6],
+                "b": [10, 12],
+                "c": [1, 2],
+            },
+            index=pd.Index([0, 1], name="time"),
+        )
+    )
+    assert aggregate_sum(intermediate_data, ["group", "time"]).equals(intermediate_data)
+    # test non-existent index column
+    with pytest.raises(KeyError):
+        aggregate_sum(intermediate_data, ["foo"])
+
+
+def test_linear_combination(intermediate_data: pd.DataFrame) -> None:
+    """Test linear combination of two columns in a multi-indexed DataFrame"""
+    assert linear_combination(intermediate_data, 1, "a", 1, "b").equals(
+        pd.Series([5, 7, 9, 11], index=intermediate_data.index)
+    )
+    assert linear_combination(intermediate_data, 2, "a", -1, "b").equals(
+        pd.Series([-2, -1, 0, 1], index=intermediate_data.index)
+    )
+    # test non-existent column
+    with pytest.raises(KeyError):
+        linear_combination(intermediate_data, 1, "a", 1, "foo")
