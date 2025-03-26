85b602284f908a9e6a9ff59d6000191a1274d911
Patrick Nast <130876799+patricktnast@users.noreply.github.com>
2025-03-26 12:09:13 -0700

    Add custom data upload (#25)
    * add custom upload
    
    * add series type
    
    * adjust error message
    
    * fix missing DataSource
    
    * move raise test to get dataset
    
    * add test for show raw dataset
    
    * lint
    
    * fix test
    
    * copy going into and out of the cache
    
    * lint
    
    * adjust args
    
    * add docstrings to "real" interface methods
    
    * don't copy on cache miss
    
    * wrap in new public method
    
    * add error messages

--- tests/automated_validation/test_interface.py
+++ tests/automated_validation/test_interface.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import pandas as pd
 import pytest
 
 from vivarium_testing_utils.automated_validation.interface import ValidationContext
@@ -9,5 +10,25 @@ from vivarium_testing_utils.automated_validation.interface import ValidationCont
 def test_add_comparison_bad_source(sim_result_dir: Path) -> None:
     """Ensure that we raise an error if the source is not recognized"""
     context = ValidationContext(sim_result_dir, None)
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match="Source bad_source not recognized"):
         context.add_comparison("cause.cause.incidence", "bad_source", "gbd")
+
+
+def test_upload_custom_data(sim_result_dir: Path) -> None:
+    """Ensure that we can upload custom data and retrieve it"""
+    context = ValidationContext(sim_result_dir, None)
+    df = pd.DataFrame({"baz": [1, 2, 3]})
+    context.upload_custom_data("foo", df)
+    assert context.get_raw_dataset("foo", "custom").equals(df)
+
+
+def test_show_raw_dataset(sim_result_dir: Path) -> None:
+    """Ensure that we can show the raw dataset"""
+    context = ValidationContext(sim_result_dir, None)
+    df = pd.DataFrame({"baz": [1, 2, 3]})
+    context.upload_custom_data("foo", df)
+
+    # Ensure loading with a string instead of a DataSource enum works
+    assert context.get_raw_dataset("foo", "custom").equals(df)
+    assert context.get_raw_dataset("deaths", "sim").shape == (8, 1)
+    assert context.get_raw_dataset("cause.cause.incidence_rate", "artifact").shape == (12, 5)
