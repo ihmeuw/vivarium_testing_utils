7f6e4dd234069da9360a1eaa405b1db7f9d6a064
Patrick Nast <pnast@uw.edu>
2025-04-08 10:57:06 -0700

    refactor measures

--- src/vivarium_testing_utils/automated_validation/measures.py
+++ src/vivarium_testing_utils/automated_validation/measures.py
@@ -11,221 +11,144 @@ from vivarium_testing_utils.automated_validation.data_schema import (
     SimOutputData,
     ArtifactData,
     CustomData,
-    IntermediateData,
-    ProcessedData,
+    RatioData,
+    MeasureData,
 )
 import pandas as pd
 from abc import ABC, abstractmethod
 
 
+class Transition:
+    """structured container to hold info about Transition Counts"""
+
+    def __init__(self, cause, start_state, end_state):
+        self.type = "transition_count"
+        self.cause = cause
+        self.data_key = f"{self.type}_{self.cause}"
+        self.start_state = start_state
+        self.end_state = end_state
+        self.transition_string = f"{self.start_state}_TO_{self.end_state}"
+        self.groupby_column = f"{self.cause}_transition"
+        self.renamed_column = f"{self.transition_string}_{self.type}"
+
+    def format_dataset(self, dataset: SimOutputData) -> SimOutputData:
+        """Clean up cause column, filter for the transition, and rename the value column."""
+        dataset = drop_redundant_column(
+            dataset,
+            "cause",
+            self.cause,
+        )
+        dataset = filter_data(dataset, {self.groupby_column: [self.transition_string]})
+        dataset = dataset.rename(columns={"value": self.renamed_column})
+        return dataset
+
+
+class PersonTime:
+    """structured container to hold info about Person Time"""
+
+    def __init__(self, cause, state=None):
+        self.type = "person_time"
+        self.cause = cause
+        self.data_key = f"{self.type}_{self.cause}"
+        self.state = state if state else "total"
+        self.groupby_column = f"{self.cause}_state"
+        self.renamed_column = f"{self.state}_{self.type}"
+
+    def format_dataset(self, dataset: SimOutputData) -> SimOutputData:
+        """Clean up cause column, filter for the state, and rename the value column."""
+        dataset = drop_redundant_column(
+            dataset,
+            "cause",
+            self.cause,
+        )
+        if self.state == "total":
+            dataset = marginalize(
+                dataset,
+                [self.groupby_column],
+            )
+        else:
+            dataset = filter_data(dataset, {self.groupby_column: [self.state]})
+        dataset = dataset.rename(columns={"value": self.renamed_column})
+        return dataset
+
+
 class Measure(ABC):
 
     @abstractmethod
-    def process_raw_sim_data(self, *args, **kwargs) -> pd.DataFrame:
-        """Process raw simulation data into a format suitable for calculations."""
+    def get_measure_data_from_artifact(self, *args, **kwargs) -> MeasureData:
+        """Process artifact data into a format suitable for calculations."""
         pass
 
     @abstractmethod
-    def compute_metric(self, intermediate_data: pd.DataFrame) -> pd.Series:
-        """Compute the metric from the processed simulation data."""
+    def get_measure_data_from_sim(self, *args, **kwargs) -> MeasureData:
+        """Process raw simulation data into a format suitable for calculations."""
         pass
 
 
-class Incidence(Measure):
+class RatioMeasure(Measure):
+    """A Measure that calculates ratio data from simulation data."""
+
+    cause: str
+    numerator: Transition | PersonTime
+    denominator: Transition | PersonTime
+
+    @abstractmethod
+    def get_ratio_data_from_sim(
+        self, numerator_data: SimOutputData, denominator_data: SimOutputData
+    ) -> RatioData:
+        """Process raw simulation data into a format suitable for calculations."""
+        pass
+
+    def get_measure_data_from_ratio(self, ratio_data: RatioData) -> MeasureData:
+        """Compute final measure data from split data."""
+        return ratio(
+            ratio_data,
+            numerator=self.numerator.renamed_column,
+            denominator=self.denominator.renamed_column,
+        )
+
+    def get_measure_data_from_sim(self, *args, **kwargs) -> MeasureData:
+        """Process raw simulation data into a format suitable for calculations."""
+        return self.get_measure_data_from_ratio(self.get_ratio_data_from_sim(*args, **kwargs))
+
+    def get_ratio_data_from_sim(
+        self, numerator_data: SimOutputData, denominator_data: SimOutputData
+    ) -> RatioData:
+        """Process raw incidence data into a format suitable for calculations."""
+        # get cause from cause index column. There should be a single value
+        return combine_sim_output_data(
+            [
+                self.numerator.format_dataset(numerator_data),
+                self.denominator.format_dataset(denominator_data),
+            ]
+        )
+
+
+class Incidence(RatioMeasure):
     """Class to compute incidence from simulation data."""
 
     def __init__(self, cause: str):
         self.cause = cause
-        self.required_data_keys = {
-            f"transition_count_{self.cause}",
-            f"person_time_{self.cause}",
-        }
-        self.transition_string = f"susceptible_to_{self.cause}_TO_{self.cause}"
-        self.state_string = f"susceptible_to_{self.cause}"
-        self.numerator_column = f"{self.transition_string}_transition_count"
-        self.denominator_column = f"{self.state_string}_person_time"
-
-    def process_raw_sim_data(
-        self, transition_count_data: pd.DataFrame, person_time_data: pd.DataFrame
-    ) -> pd.DataFrame:
-        """Process raw incidence data into a format suitable for calculations."""
-        # get cause from cause index column. There should be a single value
-        transition_count_data = drop_redundant_column(
-            transition_count_data,
-            "cause",
-            self.cause,
-        )
-        person_time_data = drop_redundant_column(
-            person_time_data,
-            "cause",
-            self.cause,
-        )
-        # filter for susceptible to infected transitions
-        transition_count_data = filter_data(
-            transition_count_data,
-            {
-                f"{self.cause}_transition": [
-                    self.transition_string,
-                ]
-            },
-        )
-        # rename value column to incident cases
-        transition_count_data = transition_count_data.rename(
-            columns={"value": self.numerator_column}
-        )
-        # filter person time for susceptible person time
-        person_time_data = filter_data(
-            person_time_data,
-            {
-                f"{self.cause}_state": [
-                    self.state_string,
-                ]
-            },
-        )
-        # rename value column to person_time
-        person_time_data = person_time_data.rename(columns={"value": self.denominator_column})
-        return combine_sim_output_data(
-            [
-                transition_count_data,
-                person_time_data,
-            ]
-        )
-
-    def compute_metric(self, intermediate_data: pd.DataFrame) -> pd.Series:
-        """Compute incidence from processed simulation data."""
-        # Compute the incidence using the transition count and person time
-        return ratio(
-            intermediate_data,
-            numerator=self.numerator_column,
-            denominator=self.denominator_column,
-        )
+        self.numerator = Transition(cause, f"susceptible_to_{self.cause}", cause)
+        self.denominator = PersonTime(cause, f"susceptible_to_{self.cause}")
 
 
-class Prevalence(Measure):
-    """Class to compute prevalence from simulation data."""
+class Prevalence(RatioMeasure):
+    """Computes prevalence from simulation data."""
 
     def __init__(self, cause: str):
         self.cause = cause
-        self.required_data_keys = {
-            f"person_time_{self.cause}",
-        }
-        self.state_string = f"{self.cause}"
-        self.denominator_column = f"{self.state_string}_person_time"
-        self.numerator_column = f"{self.cause}_prevalence"
-
-    def process_raw_sim_data(self, person_time_data: pd.DataFrame) -> pd.DataFrame:
-        """Process raw prevalence data into a format suitable for calculations."""
-        # get cause from cause index column. There should be a single value
-        person_time_data = drop_redundant_column(
-            person_time_data,
-            "cause",
-            self.cause,
-        )
-
-        # Get the person time for the cause
-        cause_person_time_data = filter_data(
-            person_time_data,
-            {
-                f"{self.cause}_state": [
-                    self.state_string,
-                ]
-            },
-        )
-        # get denominator dataframe for total person time
-        total_person_time_data = marginalize(
-            person_time_data,
-            [f"{self.cause}_state"],
-        )
-        # rename value column to person_time
-        total_person_time_data = total_person_time_data.rename(
-            columns={"value": "total_person_time"}
-        )
-        return combine_sim_output_data(
-            [
-                cause_person_time_data,
-                total_person_time_data,
-            ]
-        )
-
-    def compute_metric(self, intermediate_data: pd.DataFrame) -> pd.Series:
-        """Compute prevalence from processed simulation data."""
-        # Compute the prevalence using the person time
-        return ratio(
-            intermediate_data,
-            numerator=self.numerator_column,
-            denominator=self.denominator_column,
-        )
+        self.numerator = PersonTime(cause, cause)
+        self.denominator = PersonTime(cause)
 
 
-class Remission(Measure):
-    """Class to compute remission from simulation data."""
+class Remission(RatioMeasure):
+    """Computes remission from simulation data."""
 
     def __init__(self, cause: str):
         self.cause = cause
-        self.required_data_keys = {
-            f"transition_count_{self.cause}",
-            f"person_time_{self.cause}",
-        }
-        self.transition_string = f"{self.cause}_TO_susceptible_to_{self.cause}"
-        self.state_string = f"{self.cause}"
-        self.numerator_column = f"{self.transition_string}_transition_count"
-        self.denominator_column = f"{self.state_string}_person_time"
-
-    def process_raw_sim_data(
-        self, transition_count_data: pd.DataFrame, person_time_data: pd.DataFrame
-    ) -> pd.DataFrame:
-        """Process raw remission data into a format suitable for calculations."""
-        # get cause from cause index column. There should be a single value
-        transition_count_data = drop_redundant_column(
-            transition_count_data,
-            "cause",
-            self.cause,
-        )
-        person_time_data = drop_redundant_column(
-            person_time_data,
-            "cause",
-            self.cause,
-        )
-
-        # filter for infected to susceptible transitions
-        transition_count_data = filter_data(
-            transition_count_data,
-            {
-                f"{self.cause}_transition": [
-                    self.transition_string,
-                ]
-            },
-        )
-        # rename value column to incident cases
-        transition_count_data = transition_count_data.rename(
-            columns={"value": self.numerator_column}
-        )
-        # filter person time for susceptible person time
-        person_time_data = filter_data(
-            person_time_data,
-            {
-                f"{self.cause}_state": [
-                    self.state_string,
-                ]
-            },
-        )
-        # rename value column to person_time
-        person_time_data = person_time_data.rename(columns={"value": self.denominator_column})
-        return combine_sim_output_data(
-            [
-                transition_count_data,
-                person_time_data,
-            ]
-        )
-
-    def compute_metric(self, intermediate_data: pd.DataFrame) -> pd.Series:
-        """Compute remission from processed simulation data."""
-        # Compute the remission using the transition count and person time
-        return ratio(
-            intermediate_data,
-            numerator=self.numerator_column,
-            denominator=self.denominator_column,
-        )
+        self.numerator = Transition(cause, cause, f"susceptible_to_{self.cause}")
+        self.denominator = PersonTime(cause, f"susceptible_to_{self.cause}")
 
 
 def combine_sim_output_data(raw_datasets: list[pd.DataFrame]) -> pd.DataFrame:
