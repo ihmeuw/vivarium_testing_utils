1dfa0159b263335c4b161903cc205eefae38ef86
Patrick Nast <pnast@uw.edu>
2025-04-09 15:03:56 -0700

    add tests for comparison

--- tests/automated_validation/test_interface.py
+++ tests/automated_validation/test_interface.py
@@ -33,24 +33,49 @@ def test_show_raw_dataset(sim_result_dir: Path) -> None:
     # Ensure loading with a string instead of a DataSource enum works
     assert context.get_raw_dataset("foo", "custom").equals(df)
     assert context.get_raw_dataset("deaths", "sim").shape == (8, 1)
-    assert context.get_raw_dataset("cause.malaria.incidence_rate", "artifact").shape == (
-        12,
-        5,
+    assert context.get_raw_dataset("cause.disease.incidence_rate", "artifact").equals(
+        artifact_disease_incidence
     )
 
 
-def test___get_raw_datasets_from_source(sim_result_dir: Path) -> None:
+def test___get_raw_datasets_from_source(
+    sim_result_dir: Path,
+    transition_count_data: pd.DataFrame,
+    person_time_data: pd.DataFrame,
+    artifact_disease_incidence: pd.DataFrame,
+) -> None:
     """Ensure that we can get raw datasets from a source"""
     context = ValidationContext(sim_result_dir, None)
-    measure = Incidence("malaria")
+    measure = Incidence("disease")
     test_raw_datasets = context._get_raw_datasets_from_source(measure, DataSource.SIM)
     ref_raw_datasets = context._get_raw_datasets_from_source(measure, DataSource.ARTIFACT)
 
+    assert test_raw_datasets["numerator_data"].equals(transition_count_data)
+    assert test_raw_datasets["denominator_data"].equals(person_time_data)
+    assert ref_raw_datasets["artifact_data"].equals(artifact_disease_incidence)
 
-def test_add_comparison(sim_result_dir: Path) -> None:
+
+def test_add_comparison(
+    sim_result_dir: Path, artifact_disease_incidence: pd.DataFrame
+) -> None:
     """Ensure that we can add a comparison"""
-    measure_key = "cause.malaria.incidence_rate"
+    measure_key = "cause.disease.incidence_rate"
     context = ValidationContext(sim_result_dir, None)
     context.add_comparison(measure_key, DataSource.SIM, DataSource.ARTIFACT, [])
     assert measure_key in context.comparisons
     comparison = context.comparisons[measure_key]
+
+    assert comparison.measure.measure_key == measure_key
+    assert comparison.stratifications == []
+    expected_ratio_data = pd.DataFrame(
+        {
+            "susceptible_to_disease_to_disease_transition_count": [3, 5],
+            "susceptible_to_disease_person_time": [17, 29],
+        },
+        index=pd.Index(
+            ["A", "B"],
+            name="stratify_column",
+        ),
+    )
+    assert comparison.test_data.equals(expected_ratio_data)
+    assert comparison.reference_data.equals(artifact_disease_incidence)
