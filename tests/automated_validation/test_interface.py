cb01e8cfc56039fcd87f690c6f3a6cf8210338a7
Patrick Nast <130876799+patricktnast@users.noreply.github.com>
2025-04-14 14:26:33 -0700

    Refactor test files (#29)
    * add code changes
    
    * add binary file changes
    
    * add artifact parsing
    
    * extract DRAW_PREFIX
    
    * lint

--- tests/automated_validation/test_interface.py
+++ tests/automated_validation/test_interface.py
@@ -11,7 +11,7 @@ def test_add_comparison_bad_source(sim_result_dir: Path) -> None:
     """Ensure that we raise an error if the source is not recognized"""
     context = ValidationContext(sim_result_dir, None)
     with pytest.raises(ValueError, match="Source bad_source not recognized"):
-        context.add_comparison("cause.cause.incidence", "bad_source", "gbd")
+        context.add_comparison("cause.disease.incidence", "bad_source", "gbd")
 
 
 def test_upload_custom_data(sim_result_dir: Path) -> None:
@@ -22,7 +22,9 @@ def test_upload_custom_data(sim_result_dir: Path) -> None:
     assert context.get_raw_dataset("foo", "custom").equals(df)
 
 
-def test_show_raw_dataset(sim_result_dir: Path) -> None:
+def test_show_raw_dataset(
+    sim_result_dir: Path, artifact_disease_incidence: pd.DataFrame
+) -> None:
     """Ensure that we can show the raw dataset"""
     context = ValidationContext(sim_result_dir, None)
     df = pd.DataFrame({"baz": [1, 2, 3]})
@@ -31,4 +33,6 @@ def test_show_raw_dataset(sim_result_dir: Path) -> None:
     # Ensure loading with a string instead of a DataSource enum works
     assert context.get_raw_dataset("foo", "custom").equals(df)
     assert context.get_raw_dataset("deaths", "sim").shape == (8, 1)
-    assert context.get_raw_dataset("cause.cause.incidence_rate", "artifact").shape == (12, 5)
+    assert context.get_raw_dataset("cause.disease.incidence_rate", "artifact").equals(
+        artifact_disease_incidence
+    )
