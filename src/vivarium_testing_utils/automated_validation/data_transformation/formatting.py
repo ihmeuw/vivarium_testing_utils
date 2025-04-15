ec7e9ad2de434bbf73c99168658bed4082a8bf37
Patrick Nast <130876799+patricktnast@users.noreply.github.com>
2025-04-15 11:34:19 -0700

    Add Measures for Incidence, Prevalence, Remission (#27)
    * add pandera
    
    * add basic calc for index alignment and data filtering
    
    * stub schemas
    
    * add Measures
    
    * start interface for measures
    
    * rename some schema types
    
    * refactor measures
    
    * organize
    
    * specify FuzzyComparison
    
    * add comparison base class
    
    * revert to stub
    
    * add datasets for sim and artifact
    
    * add formatting tests
    
    * bugfixes
    
    * add tests
    
    * make test data formatting consistent
    
    * add tests of measure functions
    
    * cleanup
    
    * transform artifact data
    
    * add artifact fixture
    
    * remove old test dfs
    
    * add tests for comparison
    
    * minor fixes
    
    * lint
    
    * typing
    
    * add docstring
    
    * typing/cleanup
    
    * add docstring
    
    * isort
    
    * use normal dicts instead of LCT
    
    * refactor formatters
    
    * address comments
    
    * beef up tests
    
    * add todos
    
    * make ratio inherit from ABC

---
+++ src/vivarium_testing_utils/automated_validation/data_transformation/formatting.py
@@ -0,0 +1,74 @@
+from abc import ABC, abstractmethod
+
+import pandas as pd
+
+from vivarium_testing_utils.automated_validation.data_transformation.calculations import (
+    filter_data,
+    marginalize,
+)
+from vivarium_testing_utils.automated_validation.data_transformation.data_schema import (
+    SimOutputData,
+)
+
+
+class SimDataFormatter:
+    """A SimDataFormatter contains information about how to format particular kinds of
+    simulaton data for use in a measure calculation. For example, incidence relies on
+    both transition counts and person time data, which require different formatting/ operations
+    on assumed columns in the simulation data."""
+
+    def __init__(self, type: str, cause: str, filter_value: str) -> None:
+        self.type = type
+        self.cause = cause
+        self.data_key = f"{self.type}_{self.cause}"
+        self.redundant_columns = {
+            "measure": self.type,
+            "entity_type": "cause",
+            "entity": self.cause,
+        }
+        self.filter_column = "sub_entity"
+        self.filter_value = filter_value
+        self.new_value_column_name = f"{self.filter_value}_{self.type}"
+
+    def format_dataset(self, dataset: SimOutputData) -> SimOutputData:
+        """Clean up redundant columns, filter for the state, and rename the value column."""
+        for column, value in self.redundant_columns.items():
+            dataset = _drop_redundant_index(
+                dataset,
+                column,
+                value,
+            )
+        if self.filter_value == "total":
+            dataset = marginalize(dataset, [self.filter_column])
+        else:
+            dataset = filter_data(dataset, {self.filter_column: [self.filter_value]})
+        dataset = dataset.rename(columns={"value": self.new_value_column_name})
+        return dataset
+
+
+class TransitionCounts(SimDataFormatter):
+    """Formatter for simulation data that contains transition counts."""
+
+    def __init__(self, cause: str, start_state: str, end_state: str) -> None:
+        super().__init__("transition_count", cause, f"{start_state}_to_{end_state}")
+
+
+class PersonTime(SimDataFormatter):
+    """Formatter for simulation data that contains person time."""
+
+    def __init__(self, cause: str, state=None) -> None:
+        super().__init__("person_time", cause, state or "total")
+
+
+def _drop_redundant_index(
+    data: pd.DataFrame, idx_column_name: str, idx_column_value: str
+) -> None:
+    """Validate that a DataFrame column is singular-valued, then drop it from the index."""
+    # TODO: Make sure we handle this case appropriately when we
+    # want to automatically add many comparisons
+    if not (data.index.get_level_values(idx_column_name) == idx_column_value).all():
+        raise ValueError(
+            f"Cause {data.index.get_level_values(idx_column_name).unique()} in data does not match expected cause {idx_column_name}"
+        )
+    data = data.droplevel([idx_column_name])
+    return data
