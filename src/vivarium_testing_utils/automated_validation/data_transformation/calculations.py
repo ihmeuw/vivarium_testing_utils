07d48b532e0a4a96458bce31f34241e18efe8ed7
Patrick Nast <130876799+patricktnast@users.noreply.github.com>
2025-04-15 11:34:19 -0700

    Add Measures for Incidence, Prevalence, Remission (#27)
    * add pandera
    
    * add basic calc for index alignment and data filtering
    
    * stub schemas
    
    * add Measures
    
    * start interface for measures
    
    * rename some schema types
    
    * refactor measures
    
    * organize
    
    * specify FuzzyComparison
    
    * add comparison base class
    
    * revert to stub
    
    * add datasets for sim and artifact
    
    * add formatting tests
    
    * bugfixes
    
    * add tests
    
    * make test data formatting consistent
    
    * add tests of measure functions
    
    * cleanup
    
    * transform artifact data
    
    * add artifact fixture
    
    * remove old test dfs
    
    * add tests for comparison
    
    * minor fixes
    
    * lint
    
    * typing
    
    * add docstring
    
    * typing/cleanup
    
    * add docstring
    
    * isort
    
    * use normal dicts instead of LCT
    
    * refactor formatters
    
    * address comments
    
    * beef up tests
    
    * add todos
    
    * make ratio inherit from ABC

--- src/vivarium_testing_utils/automated_validation/data_transformation/calculations.py
+++ src/vivarium_testing_utils/automated_validation/data_transformation/calculations.py
@@ -5,16 +5,38 @@ import pandas as pd
 DataSet = TypeVar("DataSet", pd.DataFrame, pd.Series)
 
 
-def process_raw_data(
-    input_data_type: str, raw_data: pd.DataFrame, measure: str
-) -> pd.DataFrame:
-    raise NotImplementedError
+def align_indexes(datasets: list[DataSet]) -> list[DataSet]:
+    """Put each dataframe on a common index by choosing the intersection of index columns
+    and marginalizing over the rest."""
+    # Get the common index columns
+    common_index = set.intersection(*(set(data.index.names) for data in datasets))
+
+    # Marginalize over the rest
+    return [marginalize(data, common_index) for data in datasets]
 
 
-def compute_metric(
-    input_data_type: str, intermediate_data: pd.DataFrame, measure: str
-) -> pd.Series:
-    raise NotImplementedError
+def filter_data(data: DataSet, filter_cols: dict[str, list]) -> DataSet:
+    """Filter a DataFrame by the given index columns and values.
+
+    The filter_cols argument
+    should be a dictionary where the keys are column names and the values are lists of
+    values to keep. If we filter to a single value, drop the column. If the dataframe is empty
+    after filtering, raise an error."""
+    for col, values in filter_cols.items():
+        if len(values) == 1:
+            data = data[data.index.get_level_values(col) == values[0]]
+            data = data.droplevel([col])
+        else:
+            data = data[data.index.get_level_values(col).isin(values)]
+    if data.empty:
+        # TODO: Make sure we handle this case appropriately when we
+        # want to automatically add many comparisons
+        raise ValueError(
+            f"DataFrame is empty after filtering by {filter_cols}. "
+            f"Check that the filter values are valid."
+        )
+
+    return data
 
 
 def ratio(data: pd.DataFrame, numerator: str, denominator: str) -> pd.Series:
@@ -25,6 +47,8 @@ def ratio(data: pd.DataFrame, numerator: str, denominator: str) -> pd.Series:
 
 def aggregate_sum(data: DataSet, groupby_cols: list[str]) -> DataSet:
     """Aggregate the dataframe over the specified index columns by summing."""
+    if not groupby_cols:
+        return data
     return data.groupby(groupby_cols).sum()
 
 
@@ -35,7 +59,7 @@ def stratify(data: DataSet, stratification_cols: list[str]) -> DataSet:
 
 def marginalize(data: DataSet, marginalize_cols: list[str]) -> DataSet:
     """Sum over marginalize columns, keeping the rest. Syntactic sugar for aggregate."""
-    return data.groupby(data.index.names.difference(marginalize_cols)).sum()
+    return aggregate_sum(data, data.index.names.difference(marginalize_cols))
 
 
 def linear_combination(
