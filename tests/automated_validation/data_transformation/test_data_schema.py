a379a704df2fa4aee822edc78ed1f7bf40125db0
Patrick Nast <pnast@uw.edu>
2025-04-22 09:40:11 -0700

    DRY code

--- tests/automated_validation/data_transformation/test_data_schema.py
+++ tests/automated_validation/data_transformation/test_data_schema.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pandera as pa
 import pytest
 from pandera.errors import SchemaError
 
@@ -10,28 +11,32 @@ from vivarium_testing_utils.automated_validation.data_transformation.data_schema
 )
 
 
+def check_schema_error_batch(
+    schema: pa.DataFrameModel, invalid_dataframes: list[pd.DataFrame]
+) -> None:
+    """Helper function to check if a schema raises an error for invalid data."""
+    for df in invalid_dataframes:
+        with pytest.raises(SchemaError):
+            schema.validate(df)
+
+
 def test_single_numeric_value() -> None:
     """Test that the SingleNumericValue schema correctly validates a DataFrame with a single numeric column."""
-    schema = SingleNumericColumn
     data = pd.DataFrame({"value": [1, 2, 3]}, index=pd.Index([0, 1, 2], name="index"))
-    schema.validate(data)
+    SingleNumericColumn.validate(data)
 
     # Test that the schema raises an error for invalid data
-    invalid_data = data.copy()
-    invalid_data["value"] = "invalid"
-    with pytest.raises(SchemaError):
-        schema.validate(invalid_data)
+    wrong_dtype = data.copy()
+    wrong_dtype["value"] = "invalid"
 
     # Test that the schema raises an error for missing columns
-    missing_column_data = data.drop(columns=["value"])
-    with pytest.raises(SchemaError):
-        schema.validate(missing_column_data)
+    missing_column = data.drop(columns=["value"])
 
     # Test that the schema raises an error for extra columns
-    extra_column_data = data.copy()
-    extra_column_data["extra_column"] = 0
-    with pytest.raises(SchemaError):
-        schema.validate(extra_column_data)
+    extra_column = data.copy()
+    extra_column["extra_column"] = 0
+
+    check_schema_error_batch(SingleNumericColumn, [wrong_dtype, missing_column, extra_column])
 
 
 def test_sim_output(
@@ -43,80 +48,75 @@ def test_sim_output(
     levels are OK, because the sample data has the "stratify_column" index level.
     """
     assert "stratify_column" in transition_count_data.index.names
-    schema = SimOutputData
 
     # Test that the SimOutputData schema correctly validates the transition count and person time data
-    schema.validate(transition_count_data)
-    schema.validate(person_time_data)
+    SimOutputData.validate(transition_count_data)
+    SimOutputData.validate(person_time_data)
 
+    error_cases = []
     # Test that a missing index level raises an error
     for key in ["measure", "entity_type", "entity", "sub_entity"]:
         missing_index_data = transition_count_data.droplevel(key)
-        with pytest.raises(SchemaError):
-            schema.validate(missing_index_data)
+        error_cases.append(missing_index_data)
 
     # Test that out of order index levels raises an error
     out_of_order_index_data = transition_count_data.swaplevel("entity", "measure")
-    with pytest.raises(SchemaError):
-        schema.validate(out_of_order_index_data)
 
     # Test that the schema raises an error for extra columns
     extra_column_data = transition_count_data.copy()
     extra_column_data["extra_column"] = 0
-    with pytest.raises(SchemaError):
-        schema.validate(extra_column_data)
+
+    error_cases.extend([out_of_order_index_data, extra_column_data])
+
+    check_schema_error_batch(SimOutputData, error_cases)
 
 
 def test_draw_data(raw_artifact_disease_incidence: pd.DataFrame) -> None:
     """
     Test that the DrawData schema correctly validates the artifact disease incidence data.
     """
-    schema = DrawData
 
     # Test that the DrawData schema correctly validates the artifact disease incidence data
-    schema.validate(raw_artifact_disease_incidence)
+    DrawData.validate(raw_artifact_disease_incidence)
 
     # Test that a missing column does not raise an error
     missing_column_data = raw_artifact_disease_incidence.drop(columns=["draw_0"])
-    schema.validate(missing_column_data)
+    DrawData.validate(missing_column_data)
 
     # Test that an extra column raises an error
     extra_column_data = raw_artifact_disease_incidence.copy()
     extra_column_data["extra_column"] = 0
-    with pytest.raises(SchemaError):
-        schema.validate(extra_column_data)
 
     # Test that the schema raises an error for invalid data
-    invalid_data = raw_artifact_disease_incidence.copy()
-    invalid_data["draw_0"] = "invalid"
-    with pytest.raises(SchemaError):
-        schema.validate(invalid_data)
+    wrong_dtype = raw_artifact_disease_incidence.copy()
+    wrong_dtype["draw_0"] = "invalid"
+
+    check_schema_error_batch(DrawData, [extra_column_data, wrong_dtype])
 
 
 def test_ratio_data() -> None:
     """
     Test that the RatioData schema correctly validates a DataFrame with two numeric columns.
     """
-    schema = RatioData
-
     # Create a valid DataFrame
     data = pd.DataFrame({"numerator": [1, 2, 3], "denominator": [4, 5, 6]})
-    schema.validate(data)
+    RatioData.validate(data)
 
-    # Test that the schema raises an error for invalid data
-    invalid_data = data.copy()
-    invalid_data["numerator"] = "invalid"
-    with pytest.raises(SchemaError):
-        schema.validate(invalid_data)
-
-    # Test that the schema raises an error for missing columns
-    missing_column_data = data.drop(columns=["numerator"])
-    with pytest.raises(SchemaError):
-        schema.validate(missing_column_data)
+    error_cases = []
 
     # Test that the schema raises an error for extra columns
     for extra_column_value in [0, "foo"]:
         extra_column_data = data.copy()
         extra_column_data["extra_column"] = extra_column_value
-        with pytest.raises(SchemaError):
-            schema.validate(extra_column_data)
+        error_cases.append(extra_column_data)
+
+    # Test that the schema raises an error for invalid data
+    wrong_dtype = data.copy()
+    wrong_dtype["numerator"] = "invalid"
+
+    # Test that the schema raises an error for missing columns
+    missing_column_data = data.drop(columns=["numerator"])
+
+    error_cases.extend([missing_column_data, wrong_dtype])
+
+    check_schema_error_batch(RatioData, error_cases)
