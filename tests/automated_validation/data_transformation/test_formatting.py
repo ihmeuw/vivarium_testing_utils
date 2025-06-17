f348b650a4d6b55d3dcae444434972755e188ab8
Patrick Nast <pnast@uw.edu>
2025-06-17 14:47:00 -0700

    formatting

--- tests/automated_validation/data_transformation/test_formatting.py
+++ tests/automated_validation/data_transformation/test_formatting.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pandas.testing import assert_frame_equal
+
 from vivarium_testing_utils.automated_validation.data_transformation.formatting import (
     Deaths,
     RiskStatePersonTime,
