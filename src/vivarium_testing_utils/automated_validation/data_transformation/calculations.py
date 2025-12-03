5a1b9270c2b52dac1c7a263a563ad94e7cc25942
Jim Albright <albrja@uw.edu>
2025-12-02 16:44:53 -0800

    Sort index

--- src/vivarium_testing_utils/automated_validation/data_transformation/calculations.py
+++ src/vivarium_testing_utils/automated_validation/data_transformation/calculations.py
@@ -267,7 +267,7 @@ def weighted_average(
                 + list(cast_values.values()),
                 names=list(weights.index.names) + list(cast_values.keys()),
             )
-        ).reorder_levels(list(data.index.names))
+        ).reorder_levels(list(data.index.names)).sort_index()
 
     # Indexes should be equal at this point
     if not data.index.equals(weights.index):
