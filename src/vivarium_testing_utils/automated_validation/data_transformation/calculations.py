f2abdbddeb9f8f368d22a4eda8f6339305aaf19f
Jim Albright <albrja@uw.edu>
2025-12-03 12:16:58 -0800

    Move ordering

--- src/vivarium_testing_utils/automated_validation/data_transformation/calculations.py
+++ src/vivarium_testing_utils/automated_validation/data_transformation/calculations.py
@@ -267,9 +267,10 @@ def weighted_average(
                 + list(cast_values.values()),
                 names=list(weights.index.names) + list(cast_values.keys()),
             )
-        ).reorder_levels(list(data.index.names))
+        )
 
     # Sort both dataframes by their index to ensure they're in the same order
+    weights = weights.reorder_levels(list(data.index.names))
     data = data.sort_index()
     weights = weights.sort_index()
 
