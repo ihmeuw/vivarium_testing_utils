07d1a52284f093d5ee9e9a13cc9915be0fe3bc0c
Jim Albright <albrja@uw.edu>
2025-12-05 16:08:59 -0800

    Add live births formatter

--- src/vivarium_testing_utils/automated_validation/data_transformation/formatting.py
+++ src/vivarium_testing_utils/automated_validation/data_transformation/formatting.py
@@ -19,8 +19,8 @@ class SimDataFormatter:
             "entity_type",
             "entity",
         ]
-        self.filters = {"sub_entity": [filter_value]}
         self.filter_value = filter_value
+        self.filters = {"sub_entity": [self.filter_value]}
         self.name = f"{self.filter_value}_{self.measure}"
 
     def format_dataset(self, dataset: pd.DataFrame) -> pd.DataFrame:
@@ -131,3 +131,23 @@ class RiskStatePersonTime(SimDataFormatter):
 
         dataset = dataset.rename_axis(index={"sub_entity": "parameter"})
         return dataset
+
+
+class TotalLiveBirths(SimDataFormatter):
+    """Formatter for simulation data that contains total live births."""
+
+    def __init__(self) -> None:
+        self.measure = "live_births"
+        self.entity = "total"
+        # Only change
+        self.raw_dataset_name = "births"
+        self.unused_columns = [
+            "measure",
+            "entity_type",
+            "entity",
+            "sub_entity",
+        ]
+        self.filter_value = "total"
+        # NOTE: Sub entity column is currently null but will have useful values at some point
+        self.filters = {"sub_entity": [self.filter_value]}
+        self.name = f"{self.filter_value}_{self.measure}"
